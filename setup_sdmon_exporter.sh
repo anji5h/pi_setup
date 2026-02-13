5eee10da1587748efd8691723500d40d2b2827cf
anjish <40964308+anji5h@users.noreply.github.com>
2026-02-13 13:55:50 +0200

    updated setup_sdmon_exporter.sh

--- setup_sdmon_exporter.sh
+++ setup_sdmon_exporter.sh
@@ -6,7 +6,7 @@ echo "--- Deploying sdmon Exporter Files ---"
 # 1. Move the Script and make executable
 if [ -f "sdmon_exporter/sdmon_exporter.sh" ]; then
     echo "Installing script to /usr/local/bin/..."
-    sudo mv sdmon_exporter/sdmon_exporter.sh /usr/local/bin/
+    sudo cp sdmon_exporter/sdmon_exporter.sh /usr/local/bin/
     sudo chmod +x /usr/local/bin/sdmon_exporter.sh
 else
     echo "Error: 'sdmon_exporter.sh' not found in current directory."
@@ -16,7 +16,7 @@ fi
 # 2. Move Systemd Service
 if [ -f "sdmon_exporter/sdmon_exporter.service" ]; then
     echo "Installing service file..."
-    sudo mv sdmon_exporter/sdmon_exporter.service /etc/systemd/system/
+    sudo cp sdmon_exporter/sdmon_exporter.service /etc/systemd/system/
 else
     echo "Error: 'sdmon_exporter.service' not found in current directory."
     exit 1
@@ -25,7 +25,7 @@ fi
 # 3. Move Systemd Timer
 if [ -f "sdmon_exporter/sdmon_exporter.timer" ]; then
     echo "Installing timer file..."
-    sudo mv sdmon_exporter/sdmon_exporter.timer /etc/systemd/system/
+    sudo cp sdmon_exporter/sdmon_exporter.timer /etc/systemd/system/
 else
     echo "Error: 'sdmon_exporter.timer' not found in current directory."
     exit 1
